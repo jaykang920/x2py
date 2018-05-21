f8b457c293d432ac8978c1c9500eb3e1786aabec
Jae-jun Kang <jaykang920@gmail.com>
2018-05-21 14:46:43 +0900

    v0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from version import version
 setup(
     name='x2py',
     version=version,
-    description='Python 3 port of x2',
+    description='Python port of x2',
     url='https://github.com/jaykang920/x2py',
     author='Jae-jun Kang',
     author_email='jaykang920@gmail.com',
@@ -15,6 +15,7 @@ setup(
         'x2py.flows',
         'x2py.links',
         'x2py.links.asyncio',
+        'x2py.links.asyncore',
         'x2py.transforms',
         'x2py.util',
         'xpiler',

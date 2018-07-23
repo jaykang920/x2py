77ccda1b2c2a5b63c745421bcc8e8590a52b0468
Jae-jun Kang <jaykang920@gmail.com>
2018-07-23 17:16:54 +0900

    added FrameBasedFlow and TimeFlow

--- x2py/flows/__init__.py
+++ x2py/flows/__init__.py
@@ -4,3 +4,4 @@
 from x2py.flows.multi_thread_flow import MultiThreadFlow
 from x2py.flows.single_thread_flow import SingleThreadFlow
 from x2py.flows.threadless_flow import ThreadlessFlow
+from x2py.flows.time_flow import TimeFlow
